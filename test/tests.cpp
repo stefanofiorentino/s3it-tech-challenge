44ace8c8709fc986a203a2bad9819ed4c0cafbc9
Stefano Fiorentino <stefano.fiore84@gmail.com>
2018-09-16 22:20:04 +0200

    adding Catch2 as Behavior Driver Development Framework
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

---
+++ test/tests.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "libs/Catch2/single_include/catch2/catch.hpp"
+
+
+SCENARIO("first scenario ever created")
+{
+    GIVEN("a string")
+    {
+        REQUIRE(true);
+    }
+}
